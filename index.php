212e3e3ed9e8ee310a45454f81033d6a0a7e1fd5
Meris Bahti <bahtijaragic@gmail.com>
2013-08-01 22:27:37 +0200

    add rb&slim & make index.php the controller & make appropriate changes to .htaccess

--- index.php
+++ index.php
@@ -1,75 +1,13 @@
-<!DOCTYPE  HTML>
-<html lang="en">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<head>
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		
-	</head>
-	<body style="padding-top: 70px">
+<meta charset="utf-8">
+<?php require 'vendor/autoload.php';
+$app = new \Slim\Slim();
 
-		<div class="navbar navbar-inverse navbar-fixed-top bs-docs-nav">
-			<div class="container">
-				<a class="navbar-brand" href="#">MAKT</a>
-				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<div class="nav-collapse collapse bs-navbar-collapse">
-					<ul class="nav navbar-nav">
-						<li class="active">
-							<a href="#">Getting started</a>
-						</li>
-						<li>
-							<a href="#">CSS</a>
-						</li>
-						<li>
-							<a href="#">Components</a>
-						</li>
-						<li>
-							<a href="#">JavaScript</a>
-						</li>
-						<li>
-							<a href="#">Customize</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div class="container">
+$app->get('/', function() {
+    echo "Index requested!";
+});
 
-			<div class="jumbotron">
-				<h1>Hello, Meris!</h1>
-				<p>
-					Lorem ipsum....
-				</p>
-				<p>
-					<a class="btn btn-primary btn-large">Learn more</a>
-				</p>
-			</div>
-
-			<div class="jumbotron">
-				<h1>Hello, Meris!</h1>
-				<p>
-					Lorem ipsum....
-				</p>
-				<p>
-					<a class="btn btn-primary btn-large">Learn more</a>
-				</p>
-			</div>
-
-			<div class="jumbotron">
-				<h1>Hello, Meris!</h1>
-				<p>
-					Lorem ipsum....
-				</p>
-				<p>
-					<a class="btn btn-primary btn-large">Learn more</a>
-				</p>
-			</div>
-		</div>
-		
-		<script src="js/jquery.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	</body>
-</html>
+$app->get('/page/:number',function($number) {
+      echo $number;
+    });
+$app->run();
+?>
