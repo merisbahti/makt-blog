b5f9504a4c521b2b436be2383b4f8da0fd48fd64
Meris Bahti <bahtijaragic@gmail.com>
2013-08-01 22:56:21 +0200

    add route to template from index controller

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <?php require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
-$app->get('/', function() {
-    echo "Index requested!";
-});
+$app->get('/', function() use ($app) {
+      $app->render('index-template.html');
+    });
 
 $app->get('/page/:number',function($number) {
       echo $number;
