d1ee9e9b6ef84e9936b681b9b75d275207217558
Meris Bahti <bahtijaragic@gmail.com>
2013-08-02 02:17:18 +0200

    integrate mock-db post with news-template

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 <?php require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
+include("rb/rb.php");
+
+R::setup("sqlite:db.db");
+
 $app->get('/', function() use ($app) {
+      $news = R::findAll('news',' ORDER BY id LIMIT 10 ');
       $view = $app->view();
       $view->setData("foo", "bar");
+      $view->setData("app", $app);
+      $view->setData("news", $news);
+      $view->setData("view", $view);
       $app->render('index-template.html');
     });
 
