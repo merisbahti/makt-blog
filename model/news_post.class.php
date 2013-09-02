7391bff3a5d35004637143c28203ea056ca6303c
3amice <bahtijaragic@gmail.com>
2013-09-02 21:30:54 +0200

    add abstract media render function to news_post

--- model/news_post.class.php
+++ model/news_post.class.php
@@ -2,9 +2,13 @@
 abstract class news_post {
 	function __construct($title, $subtext, $body, $date, $author) {	
 		$this->title		= $title;
-		$this->subtext	= $subtext;
+		$this->subtext		= $subtext;
 		$this->body			= $body;
 		$this->date 		= $date;
 		$this->author		= $author;
 	}
+	/*
+	* Returns a HTML Blob with relevant media content.
+	*/
+	abstract function render_content(); 
 }
