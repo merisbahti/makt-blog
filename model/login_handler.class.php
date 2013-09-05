3377e0beb212d58e6a536fe96cc2b4101115d6c8
unknown <bahtijaragic@gmail.com>
2013-09-05 18:27:26 +0200

    add exception handling if database fucks up.

--- model/login_handler.class.php
+++ model/login_handler.class.php
@@ -1,4 +1,4 @@
-<?
+<?php
 class login_handler {
 	/*
 	* Checks DB if valid user & pass.
@@ -6,25 +6,29 @@ class login_handler {
 	* returns false if any exception.
 	*/
 	public static function login($username, $pass) {
-		$user_bean = R::findOne('user', 'username = ? ', array($username));
-		if (!is_null($user_bean) && hash('sha512', $pass) == $user_bean->pass) {
-			echo session_id();
-			if (is_null(R::findOne('session', 'session_id = ?', array(session_id())))) {
+    try {
+      $user_bean = R::findOne('user', 'username = ? ', array($username));
+      if (!is_null($user_bean) && hash('sha512', $pass) == $user_bean->pass) {
+       echo session_id();
+       if (is_null(R::findOne('session', 'session_id = ?', array(session_id())))) {
 				// Logging out old user from system.
-				$same_user_session = R::findOne('session', 'username = ?', array($username));
-				if (!is_null($same_user_session)) {
-					R::trash($same_user_session);
-				}
-				$session = R::dispense('session');
-				$session->username  		= $username;
-				$session->session_id 	= session_id();
-				R::store($session);
-			}
-      return true;
-		} else {
-      return false;
-		}
-	}
+        $same_user_session = R::findOne('session', 'username = ?', array($username));
+        if (!is_null($same_user_session)) {
+         R::trash($same_user_session);
+       }
+       $session = R::dispense('session');
+       $session->username  		= $username;
+       $session->session_id 	= session_id();
+       R::store($session);
+     }
+     return true;
+   } else {
+    return false;
+  }
+} catch (exception $e) {
+  return false;
+}
+}
 	/*
 	* Unconditional log-out, destroy session and remove from database.
 	*/
@@ -46,3 +50,4 @@ class login_handler {
 	}
 
 }
+?>
