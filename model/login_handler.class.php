cf9a54dc980e2644bbfbad2ae1b9950621d46377
unknown <bahtijaragic@gmail.com>
2013-09-05 18:15:38 +0200

    fix text to bool

--- model/login_handler.class.php
+++ model/login_handler.class.php
@@ -1,7 +1,5 @@
 <?
-
 class login_handler {
-	
 	/*
 	* Checks DB if valid user & pass.
 	* Sets OR returns session id. Probably sets. Kthxbai.
@@ -12,25 +10,21 @@ class login_handler {
 		if (!is_null($user_bean) && hash('sha512', $pass) == $user_bean->pass) {
 			echo session_id();
 			if (is_null(R::findOne('session', 'session_id = ?', array(session_id())))) {
-
 				// Logging out old user from system.
 				$same_user_session = R::findOne('session', 'username = ?', array($username));
 				if (!is_null($same_user_session)) {
 					R::trash($same_user_session);
 				}
-
-
 				$session = R::dispense('session');
 				$session->username  		= $username;
 				$session->session_id 	= session_id();
 				R::store($session);
 			}
+      return true;
 		} else {
-			echo "Invalid login.";
+      return false;
 		}
-
 	}
-
 	/*
 	* Unconditional log-out, destroy session and remove from database.
 	*/
@@ -39,18 +33,16 @@ class login_handler {
 		$session = R::findOne('session', 'session_id = ? ',array($id));
 		session_destroy();
 		if (!is_null($session))
-		R::trash($session);
-	}
-
-	
+      R::trash($session);
+  }
 	/*
 	* Checks DB if this is a valid session.
 	* Returns boolean.
 	*/
 	public static function verify() {
 		$session_bean = R::findOne('session', 'session_id = ?', array(session_id()));
-		if (!is_null($session_bean)) return "true<br>"; 
-		return "false<br>";
+		if (!is_null($session_bean)) return true; 
+		return false;
 	}
 
 }
